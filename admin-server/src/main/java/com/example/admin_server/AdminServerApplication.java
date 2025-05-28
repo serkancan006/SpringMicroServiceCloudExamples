522fe5fbed5bb9f48157b9858ed27f5afc8dc50a
sekocan <sekocan@DESKTOP-QBMATUH>
2025-05-28 15:45:53 +0300

    add admin server

--- admin-server/src/main/java/com/example/admin_server/AdminServerApplication.java
+++ admin-server/src/main/java/com/example/admin_server/AdminServerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableAdminServer
 public class AdminServerApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServerApplication.class, args);
 	}
